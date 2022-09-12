7ad56e0e25285e9b189704131d31ba9afc773c50
michellejanosi <michelle.janosi@gmail.com>
2022-09-12 05:24:22 -0400

    Add functioning button

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-fetch('http://www.boredapi.com/api/activity/')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
+document.getElementById('get-activity').addEventListener('click', () => {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('.activity').innerHTML = `<p>${data.activity}</p>`
+    });
+})
